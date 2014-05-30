aa5e3a9a91dc0240c7a7182e07592721f09d71f3
garrett dalton <garrettjdalton@gmail.com>
2014-05-29 21:03:13 -0400

    week3challenge3

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,19 +1,44 @@
-// I paired [by myself, with:] on this challenge.
+// I worked by myself on this challenge.
 
 // __________________________________________
 // Write your code below.
+var secretNumber = 1;
+var secretNumber = 7;
+var password = "string";
+var password = "just open the door.";
+var allowedIn = true;
+var allowedIn = false;
+var members = [];
+members [0] = "John";
+members [3] = "Mary";
 
 
 
 
 
+// // __________________________________________
+// // Reflection: Use the reflection guidelines to write a reflection here. 
+// // What parts of your strategy worked? What problems did you face?
+// //The strategy was straightforward on this challenge, keep adding to the code so the test code came up true.
 
+// What questions did you have while coding? What resources did you find to help you answer them?
+// //It took me a little bit to understand that the test code was just for testing, and that I had to create my own code to have them work.  I thought I was editing the snippets at first.  I solved this problem by taking a minute to break then re reading the instructions.
 
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+// //I didn't have any trouble on this challenge, everything clicked once I realized I had to start my code from scratch.
 
-// __________________________________________
-// Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
+// Did you learn any new skills or tricks?
+// //I learned the reasoning and good techniques to use test code.
+
+// How confident are you with each of the Learning Competencies?
+// //100% confident
+
+// Which parts of the challenge did you enjoy?
+// //I liked that we started to get into the meat and potatoes of javascript.  Reading about it doesn't always get the syntax ingrained in my head.
+
+// Which parts of the challenge did you find tedious?
+// //None
+// // 
 // 
 // 
 // 
