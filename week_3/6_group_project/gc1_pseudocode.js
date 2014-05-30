af7c5116a42d82ba471706f1eca4890d575bacd3
garrett dalton <garrettjdalton@gmail.com>
2014-05-30 01:24:49 -0400

    changed part1 of week3 group project

--- week_3/6_group_project/gc1_pseudocode.js
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -2,14 +2,11 @@
 
 // Here is my part of the challenge:
 
-//I want a tool called 'sum' which can do two things:
-// 1. Take a list of numbers (the amount of numbers in that list being odd) and give me the sum of the listed numbers
-// 2. Take a list of numbers (the amount of numbers in that list being even) and give me the sum of the listed numbers
+//I want a tool called 'sum' which can do the following:
+// Take a list of numbers (the amount of numbers in the list can vary) and give me the sum of those numbers
 
-//I want a tool called 'mean' which can do two things:
-// 1. Take a list of numbers (the amount of numbers in that list being odd) and give me the average of the listed numbers
-// 2. Take a list of numbers (the amount of numbers in that list being even) and give me the average of the listed numbers
+//I want a tool called 'mean' which can do the following:
+// Take a list of numbers (the amount of numbers in the list can vary) and give me the average of those numbers
 
-//I want a tool called 'median' which can do two things:
-// 1. Take a list of numbers (the amount of numbers in that list being odd) and give me the median of the listed numbers
-// 2. Take a list of numbers (the amount of numbers in that list being even) and give me the median of the listed numbers
+//I want a tool called 'median' which can do the following:
+// Take a list of numbers (the amount of numbers in the list can vary) and give me the sum of those numbers
