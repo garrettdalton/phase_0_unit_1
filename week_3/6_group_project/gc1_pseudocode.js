d358850171ef4a92d9c7e9e8d2f580a29c663bb0
garrett dalton <garrettjdalton@gmail.com>
2014-06-02 00:11:23 -0400

    added group project reflection

--- week_3/6_group_project/gc1_pseudocode.js
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -10,3 +10,44 @@
 
 //I want a tool called 'median' which can do the following:
 // Take a list of numbers (the amount of numbers in the list can vary) and give me the sum of those numbers
+
+
+
+
+//What parts of your strategy worked? What problems did you face?
+// I didn't really encounter many problems, as person 1, the only real issue was waiting for the other members to submit.
+// It wasn't much of an issue though, as we seemed to communicate well and get our parts done in time.  It definitely was
+// a highlight as to how much communication is needed in programming.
+
+// What questions did you have while coding? What resources did you find to help you answer them?
+// I didn't have questions as I just had to generate user stories from test code.
+
+// What concepts are you having trouble with, or did you just figure something out? If so, what?
+// I figured out how important pseudocode is, and just how important it is for every step of the coding
+// process needs to be accessible by multiple people.
+
+// Did you learn any new skills or tricks?
+// Haha, I learned how to reply all on emails, which is very useful!!
+
+// How confident are you with each of the Learning Competencies?
+// I would say I am confident in them, though I worry a little about my pseudocode.
+
+// Which parts of the challenge did you enjoy?
+// I enjoyed watching the communication all come together.
+
+// Which parts of the challenge did you find tedious?
+// Working up the courage to be person 1!  I was sweating about whether or not my user stories were readable.
+
+// What was this experience like?
+// It was definitely unique, It was kinda scary relying on other people and having other people relying on you.
+// I was so nervous being user 1, I worried that I would mess the whole thing up.
+
+// Was your group successful in passing the tests?
+// Our group completed each test successfully.
+
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+// I learned that user stories need to be expertly written so that many different people can understand them.
+// I learned the importance of things being clear, concise, and accessible at each and every step of developent.
+// I had never really thought pseudocode was very important, and mine is often not very neat or acessible.  Now I
+// realize how neccesary it is that it be communicable.
+
